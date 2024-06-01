fc2b7c0dcb7566f915d58c1dde9f45a301b5ba2b
skyglass <skyglass2001@gmail.com>
2024-06-01 18:43:21 +0200

    test change

--- client/components/header.js
+++ client/components/header.js
@@ -22,7 +22,7 @@ export default ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link className="navbar-brand" href="/">
-        GitTix
+        SkyComposer
       </Link>
 
       <div className="d-flex justify-content-end">
